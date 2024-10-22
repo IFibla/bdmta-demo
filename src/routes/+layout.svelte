aed4b2e882eae511a1e9be2e7ccf0738179108f8
ignasi.fibla <ignasi.fibla@est.fib.upc.edu>
2024-10-22 15:31:30 +0200

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import Textfield from '@smui/textfield';
+    import SegmentedButton, { Segment, Label } from '@smui/segmented-button';
+    let choices = ['Low', 'Medium', 'High'];
+    let valueA = "jasd";
+</script>
+
+<svelte:head>
+	<title>Strategy Simulator</title>
+</svelte:head>
+
+<div id="central-slot">
+    <h2>Strategy Simulator</h2>
+    <h6>Big Data Management, Technologies and Analytics</h6>
+
+    <div id="input-bar">
+        <Textfield bind:value={valueA} label="Number of laps" />
+        <Textfield bind:value={valueA} label="Target lap time" />
+        <Textfield bind:value={valueA} label="Average pit stop loss" />
+    </div>
+
+    <SegmentedButton segments={choices} let:segment singleSelect>
+        <Segment {segment}>
+        <Label>{segment}</Label>
+        </Segment>
+    </SegmentedButton>
+
+</div>
+
+<style>
+    #central-slot {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+        row-gap: 10px;
+	}
+
+    #input-bar {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        column-gap: 10px;
+        width: 100%;
+    }
+</style>
